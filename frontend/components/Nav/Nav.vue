346897994edfa243ff3adef7ea2da90d347ac732
uspenskii <adndrei.uspenskii@gmail.com>
2021-09-08 21:19:27 +0300

    Front update

---
+++ frontend/components/Nav/Nav.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+ul.nav
+  li.nav__item
+    nuxt-link.nav__link(to='/') Главная
+  li.nav__item
+    nuxt-link.nav__link(to='/') Каталог
+  li.nav__item
+    nuxt-link.nav__link(to='/') Портфолио
+  li.nav__item
+    nuxt-link.nav__link(to='/') Цены
+  li.nav__item
+    nuxt-link.nav__link(to='/') Акции
+  li.nav__item
+    nuxt-link.nav__link(to='/') Отзывы
+  li.nav__item
+    nuxt-link.nav__link(to='/') Контакты
+</template>
+
+<script>
+
+</script>
+
+<style src='./nav.scss' lang="scss">
+
+</style>
---
+++ frontend/nuxt.config.js
@@ -0,0 +1,43 @@
+export default {
+  server: {
+    port: 8004,
+    // host: '0.0.0.0',
+  },
+
+  target: 'server',
+
+  ssr: true,
+
+  head: {
+    title: 'default-nuxt',
+    htmlAttrs: {
+      lang: 'ru-RU'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, { rel: 'stylesheet',  href: 'https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap' }
+    ]
+    
+    
+  },
+
+  buildModules: ['nuxt-compress'],
+  
+  css: ['@/assets/scss/app.scss'],
+
+  styleResources: {
+    scss: ['./assets/scss/*.scss'],
+  },
+
+  plugins: [{ src: '~/plugins/plugins.js', mode: 'client' }, '~plugins/api.js'],
+
+  components: true,
+
+  modules: ['@nuxtjs/axios', '@nuxtjs/style-resources', '@nuxtjs/svg-sprite'],
+    
+}
