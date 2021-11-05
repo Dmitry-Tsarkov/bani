fa901ff59f01961bd18db1ac792b2e98866bee0a
uspenskii <adndrei.uspenskii@gmail.com>
2021-11-06 01:42:20 +0300

    Front

---
+++ frontend/pages/feedback.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.page__content 
+  .container
+    Breadcrumbs(:data='breadcrumbs')
+    Headline(title='Форма заказа')
+    FormOrder
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    breadcrumbs() {
+      let breadcrumbs = [
+        {
+          title: 'Форма заказа',         
+        },               
+      ]
+
+      return breadcrumbs
+    },
+  },
+  // asyncData(context) {
+    
+  //   return context.$api.load(
+  //     `actions/${context.route.params.slug}`
+  //   )
+  // },
+}
+</script>
